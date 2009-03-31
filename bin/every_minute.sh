9017f884ebb1f2e2c702c4a866d2b44ac890c3e9
Brian Rosner <brosner@gmail.com>
2009-03-31 15:39:48 -0500

    Added cron script for mail and notices.

---
+++ bin/every_minute.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source /home/pinax/virtualenvs/cpc/bin/activate
+cd /home/pinax/webapps/code.pinaxproject.com/cpc_project
+./manage.py emit_notices >> /home/pinax/code.pinaxproject.com/logs/cron_notices.log 2>&1
+./manage.py send_mail >> /home/pinax/webapps/code.pinaxproject.com/logs/cron_mail.log 2>&1
