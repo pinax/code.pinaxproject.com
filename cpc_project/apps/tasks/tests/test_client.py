85c05e8924c381d3943108d84e6353ee5a8d9a88
Daniel Greenfeld <pydanny@gmail.com>
2009-05-08 11:26:14 -0400

    Test for ticket #128

--- cpc_project/apps/tasks/tests/test_client.py
+++ cpc_project/apps/tasks/tests/test_client.py
@@ -1,29 +1,57 @@
 # coding: utf-8
 
 from django.test import TestCase
-from django.test.client import Client
 
+
+rst_markup = """
+Sample Header
+===============
+
+Blah blah blah
+
+Lower Header
+-------------
+
+Blah blah blah
+"""
 
 class TestAddForm(TestCase):
+    fixtures = ['test_tasks.json']    
     
     def setUp(self):
-        # Every test needs a client
-        self.client = Client()
+        pass
     
     def tearDown(self):
         pass
     
     def test_add_buttons(self):
-         response = self.client.get('/tasks/add/')
+        response = self.client.get('/tasks/add/')
+
+        # Check that the response is 200 OK.
+        self.failUnlessEqual(response.status_code, 200)
+
+        # check that there is an add button
+        self.assertContains(response, '<input type="submit" value="Add task"/>')
+
+        # check that there is an add another task button
+        self.assertContains(response, 'add-another-task')
          
-         # Check that the response is 200 OK.
-         self.failUnlessEqual(response.status_code, 200)
-         
-         # check that there is an add button
-         self.assertContains(response, '<input type="submit" value="Add task"/>')
-         
-         # check that there is an add another task button
-         self.assertContains(response, 'add-another-task')
-         
-         
-         
+    def test_markup(self):
+
+        # log in so the user has task add rights.
+        self.client.login(username='admin', password='test')        
+        
+        # create some sample form data
+        form_data = {'summary': 'my simple test', 
+                'detail': rst_markup,
+                'markup':'rst',
+                'assignee':'',
+                'tags':''
+                }
+                
+        # post the form
+        response = self.client.post('/tasks/add/', form_data)
+        
+        response = self.client.get('/tasks/task/3/')
+        
+        self.assertContains(response, '<h1 class="title">Sample Header</h1>')
