85e2311a31cd90f79a8a4383d85e30b5d5ffa13f
Brian Rosner <brosner@gmail.com>
2009-08-08 00:32:02 -0600

    added back return false on click for marking a response as accepted

--- cpc_project/media/js/questions.js
+++ cpc_project/media/js/questions.js
@@ -2,4 +2,5 @@ $("a.mark_as_accepted").click(function() {
     $(".accepted").removeClass("accepted");
     response = $(this).parents("div.response");
     response.addClass("accepted");
+    return false;
 });
