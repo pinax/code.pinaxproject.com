30fe95e3d0adbdb5cdef56837734375063d0189a
Brian Rosner <brosner@gmail.com>
2009-08-08 03:25:30 -0600

    mark response accepted after the http request has returned

--- cpc_project/media/js/questions.js
+++ cpc_project/media/js/questions.js
@@ -1,7 +1,9 @@
 $("li.mark_as_accepted > a").click(function() {
-    $.get(this.href);
-    $(".accepted").removeClass("accepted");
-    response = $(this).parents("div.response");
-    response.addClass("accepted");
+    var anchor = $(this);
+    $.get(this.href, function() {
+        $(".accepted").removeClass("accepted");
+        response = anchor.parents("div.response");
+        response.addClass("accepted");
+    });
     return false;
 });
