05ec8d394aa10068adf5eb45149e1deebce2b426
Brian Rosner <brosner@gmail.com>
2011-03-16 23:29:46 -0400

    updated to django-notification==0.3a1.dev2

--- cpc_project/migrations/004_add_notification.sql
+++ cpc_project/migrations/004_add_notification.sql
@@ -23,19 +23,5 @@ CREATE TABLE "notification_noticequeuebatch" (
     "pickled_data" text NOT NULL
 )
 ;
-### New Model: notification.ObservedItem
-CREATE TABLE "notification_observeditem" (
-    "id" serial NOT NULL PRIMARY KEY,
-    "user_id" integer NOT NULL REFERENCES "auth_user" ("id") DEFERRABLE INITIALLY DEFERRED,
-    "content_type_id" integer NOT NULL REFERENCES "django_content_type" ("id") DEFERRABLE INITIALLY DEFERRED,
-    "object_id" integer CHECK ("object_id" >= 0) NOT NULL,
-    "notice_type_id" integer NOT NULL REFERENCES "notification_noticetype" ("id") DEFERRABLE INITIALLY DEFERRED,
-    "added" timestamp with time zone NOT NULL,
-    "signal" text NOT NULL
-)
-;
 CREATE INDEX "notification_noticesetting_user_id" ON "notification_noticesetting" ("user_id");
 CREATE INDEX "notification_noticesetting_notice_type_id" ON "notification_noticesetting" ("notice_type_id");
-CREATE INDEX "notification_observeditem_user_id" ON "notification_observeditem" ("user_id");
-CREATE INDEX "notification_observeditem_content_type_id" ON "notification_observeditem" ("content_type_id");
-CREATE INDEX "notification_observeditem_notice_type_id" ON "notification_observeditem" ("notice_type_id");
