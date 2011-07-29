c42eda33bce7c48e75048a951e8a1aa124572b83
Patrick Altman <paltman@gmail.com>
2011-07-29 12:55:31 -0500

    turned email on and debug off

--- cpc_project/deploy/settings_primary.py
+++ cpc_project/deploy/settings_primary.py
@@ -1,1 +1,10 @@
 SITE_ID = 2
+
+DEBUG=False
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.sendgrid.net"
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "sendgrid@eldarion.com"
+EMAIL_HOST_PASSWORD = "at8di4op5va"
+EMAIL_USE_TLS = True
